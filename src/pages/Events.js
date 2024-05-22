3da91b1241ea5c4a0fc38700002aa9ed1f908f44
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-22 21:49:56 +0530

    Add src/pages/Events.js

---
+++ src/pages/Events.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Events = () => {
+  return (
+    <div>
+      <h1>Events</h1>
+      {/* Add events page content here */}
+    </div>
+  );
+};
+
+export default Events;
