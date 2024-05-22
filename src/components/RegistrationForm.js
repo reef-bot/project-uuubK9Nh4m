9a9dbd8e268f76db3c2f8a1596cabf1ba9f0c839
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-22 21:49:55 +0530

    Add src/components/RegistrationForm.js

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegistrationForm = () => {
+  return (
+    <div>
+      <h1>Registration Form</h1>
+      {/* Add registration form fields here */}
+    </div>
+  );
+};
+
+export default RegistrationForm;
